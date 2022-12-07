b1d52c047f77ccbebad7eeb30030dbc8f6dd1e54
李泽 <lize@144.121.140.150.lightower.net>
2022-12-07 10:50:46 -0500

    add new gif by showing different frame

--- lcd/main.c
+++ lcd/main.c
@@ -1,13 +1,18 @@
-
 #include "DEV_Config.h"
 #include "st7735_driver.h"
+#include "show_music.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
     ST7735_Init();
+    int i = 0;
     while(true){
-    ST7735_DrawImage(0, 0, 80, 160, arducam_logo);
-    sleep_ms(1000);
+    if(i==11){
+        i=0;
+    }
+    ST7735_DrawImage(0, 0, 80, 160, music[i]);
+    i++;
+    sleep_ms(100);
     }
 }
