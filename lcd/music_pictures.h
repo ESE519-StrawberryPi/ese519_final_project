8c8bfd302163c572caf25828171ea9ccf071c921
李泽 <lize@144.121.140.150.lightower.net>
2022-12-07 15:00:43 -0500

    add 3 gifs

---
+++ lcd/music_pictures.h
@@ -0,0 +1,12 @@
+/* vim: set ai et ts=4 sw=4: */
+#ifndef __MUSIC_PICTURES_H__
+#define __MUSIC_PICTURES_H__
+
+#include <stdint.h>
+
+
+extern const uint8_t dou[22][25608];
+extern const uint8_t music_color[3][25608];
+extern const uint8_t note[20][25608];
+
+#endif // __MUSIC_PICTURES_H__
--- lcd/show_music.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SHOW_MUSIC_H__
-#define __SHOW_MUSIC_H__
-
-#include <stdint.h>
-
-
-extern const uint8_t music[10][8200];
-
-#endif // __SHOW_MUSIC_H__
