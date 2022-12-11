82f329bb91a14dec81203d6391058edbd2b1a03d
TomGoh <haozewu@seas.upenn.edu>
2022-12-11 18:38:30 -0500

    Finish the all basic functions

--- utils/play.h
+++ utils/play.h
@@ -5,11 +5,14 @@
 #include "../config.h"
 #include "pwm_utils.h"
 
+
 #ifndef FINAL_PROJECT_PLAY_H
 #define FINAL_PROJECT_PLAY_H
 
 void play_mode(PIO pio, uint sm,uint8_t *data, int dataLen);
 void record_music(int charArrLen);
 void load_music(int charArrLen, PIO pio, uint sm, uint32_t period);
+void play_mode_no_param();
+void core1_entry();
 
 #endif //FINAL_PROJECT_PLAY_H
--- utils/play_mode.c
+++ utils/play_mode.c
@@ -4,6 +4,9 @@
 
 #include "play.h"
 #include "sl_utils.h"
+#include "pico/multicore.h"
+#include "../resources/audio.h"
+
 
 // play the given music data
 void play_mode(PIO pio, uint sm, uint8_t *data, int dataLen){
@@ -29,19 +32,60 @@ void play_mode(PIO pio, uint sm, uint8_t *data, int dataLen){
 
 
         // if (position < (DATA_LENGTH << 3) - 1){
-        if ( position < (dataLen << 2)-1){
-            pio_pwm_set_level(pio0, sm, data[position>>2]);
+        if (position < (dataLen << 2) - 1) {
+            pio_pwm_set_level(pio0, sm, data[position >> 2]);
             position++;
-        }else{
+        } else {
             position = 0;
-            char c = get_key_timeout_us(1*1000*1500);
-            printf("%c",c);
-            if (c=='#'){
+            char c = get_key_timeout_us(1 * 1000 * 1500);
+            printf("%c\n", c);
+            if (c == '#') {
                 return;
             }
-            pio_pwm_set_level(pio0,sm, 0);
+            pio_pwm_set_level(pio0, sm, 0);
             printf("\nReset the position");
             printf("\nRestart again");
         }
     }
 }
+
+void play_mode_no_param(){
+    PIO pio = pio0;
+    uint sm = 0;
+    uint offset = pio_add_program(pio, &pwm_program);
+    pwm_program_init(pio, sm, offset, AUDIO_PIN);
+
+    play_mode(pio, sm, DATA_AUDIO, DATA_LENGTH_AUDIO);
+
+    printf("Complete subtask.");
+}
+
+void core1_entry() {
+    while (1) {
+        // Function pointer is passed to us via the FIFO
+        // We have one incoming int32_t as a parameter, and will provide an
+        // int32_t return value by simply pushing it back on the FIFO
+        // which also indicates the result is ready.
+        int32_t (*func)() = (int32_t(*)()) multicore_fifo_pop_blocking();
+//        int32_t p = multicore_fifo_pop_blocking();
+//        int32_t result = (*func)(p);
+//        multicore_fifo_push_blocking(result);
+
+
+        (*func)();
+    }
+}
+
+//int main(){
+//    stdio_init_all();
+//    printf("Hello, multicore_runner!\n");
+//
+//    multicore_launch_core1(core1_entry);
+//
+//    // push the param and func into stack
+//    multicore_fifo_push_blocking((uintptr_t) &play_mode_no_param);
+//
+//    printf("Complete main thread.")
+//    // push *data and dataLen
+////    multicore_fifo_push_blocking(TEST_NUM);
+//}
